005750969a50d51c244fe8667f8c4671bdacc7c8
JP <cala.js@icloud.com>
2022-08-13 10:27:11 -0500

    Minor refactoring

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express'
 import { connect } from '../dbconnection'
-
+// import { Medicine } from '../interfaces/medicine.interface'
 const table: string = 'medicines'
 
-export async function getProducts(_req: Request, res: Response) {
+export async function getProducts(_req: Request, res: Response): Promise<Response> {
     const conn = await connect()
     const products = await conn.query(`SELECT * FROM ${table} WHERE quantity > 0`)
     return res.json(products[0])
